853b4163a9fbc07b1e2591235d05ad8862c6a1d4
anicktoo <anickt00@gmail.com>
2023-08-14 23:47:02 +0300

    ejs-views added

--- server.js
+++ server.js
@@ -2,20 +2,33 @@ const express = require('express');
 const path = require('path');
 
 const PORT = 3000;
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`Listening port ${PORT}`);
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(createPath('index'));
+    res.render(createPath('index'));
 });
 
 app.get('/contacts', (req, res) => {
-    res.sendFile(createPath('contacts'));
+    res.render(createPath('contacts'));
+});
+
+app.get('/posts/:id', (req, res) => {
+    res.render(createPath('post'));
+});
+
+app.get('/posts', (req, res) => {
+    res.render(createPath('posts'));
+});
+
+app.get('/add-post', (req, res) => {
+    res.render(createPath('add-post'));
 });
 
 app.get('/about-us', (req, res) => {
@@ -27,5 +40,5 @@ app.get('/about-us', (req, res) => {
 app.use((req, res) => {
     res
         .status(404)
-        .sendFile(createPath('error'));
+        .render(createPath('error'));
 });
